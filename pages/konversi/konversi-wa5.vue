262a8ed564b03a4ab790c983d0146c2f8dc20031
dhytalu <nurdita07@gmail.com>
2025-01-06 11:11:17 +0700

    add page konversi wa5

---
+++ pages/konversi/konversi-wa5.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <div class="text-red-500">{{ data }}</div>
+    </div>  
+  </template>
+  
+  <script lang="ts" setup>
+    const client = useSanctumClient()
+    const { data, error, refresh } = await useAsyncData('konversi-wa5', fetchData)
+    function fetchData() {
+      return client(`/api/konversi-wa5`);
+    }
+  </script>
+  
+  
