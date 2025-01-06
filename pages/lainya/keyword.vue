4b115caf089e93c3e9f85fc3be16edff38832365
dhytalu <nurdita07@gmail.com>
2025-01-06 13:06:12 +0700

    add page keyword

---
+++ pages/lainya/keyword.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <div class="text-red-500">{{ data }}</div>
+    </div>  
+  </template>
+  
+  <script lang="ts" setup>
+    const client = useSanctumClient()
+    const { data, error, refresh } = await useAsyncData('keyword', fetchData)
+    function fetchData() {
+      return client(`/api/keyword`);
+    }
+  </script>
+  
+  
