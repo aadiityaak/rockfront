1deee8dc6ee8e3565f76786dc90c8f5362f8e867
dhytalu <nurdita07@gmail.com>
2025-01-06 10:44:35 +0700

    add page saldo bank, ringkasan, rekap chat

---
+++ pages/rekap-chat/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <div class="text-fuchsia-500">{{ data }}</div>
+    </div>  
+  </template>
+  
+  <script lang="ts" setup>
+    const client = useSanctumClient()
+    const { data, error, refresh } = await useAsyncData('rekap-chat', fetchData)
+    function fetchData() {
+      return client(`/api/rekap-chat`);
+    }
+  </script>
+  
+  
