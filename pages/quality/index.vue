0fc0a6e358d8c7757a380a52c0140a84c174b901
dhytalu <nurdita07@gmail.com>
2025-01-06 10:49:57 +0700

    add quality page

---
+++ pages/quality/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <div class="text-fuchsia-500">{{ data }}</div>
+    </div>  
+  </template>
+  
+  <script lang="ts" setup>
+    const client = useSanctumClient()
+    const { data, error, refresh } = await useAsyncData('quality', fetchData)
+    function fetchData() {
+      return client(`/api/quality`);
+    }
+  </script>
+  
+  
